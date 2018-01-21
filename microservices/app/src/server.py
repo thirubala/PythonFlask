96ab0b69797f2187ecc05cdcea332102656b8103
thirubala3171@ssh.imad.hasura-app.io <thirubala31@gmail.com>
2018-01-21 20:50:58 +0530

    updated the url /Thiru-taskone

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -7,7 +7,14 @@ def home():
     return "Hasura Hello World"
 
 # Uncomment to add a new URL at /new
-
+#task-one-one
 @app.route('/Thiru-taskone')
 def hello_world():
-    return 'Hello World - Thiru'
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+	posts = requests.get('https://jsonplaceholder.typicode.com/posts').json()
+	return render_template('Task2.html',users=users,posts=posts)
+			
+	return "Done"return 'Hello World - Thiru'
+
+	
+	
