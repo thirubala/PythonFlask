44fd02d9436a54c3d200f830e700f1150e436008
thirubala3171@ssh.imad.hasura-app.io <thirubala31@gmail.com>
2018-01-31 23:08:01 +0530

    Removed the old code

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -3,51 +3,49 @@ from flask import jsonify
 from flask import json
 from flask import Flask,request,render_template,make_response
 
+var hasuradbUrl = "https://data.joviality89.hasura-app.io/v1/query";
+
 #task-two
 @app.route("/")
 def home():
-    return 'Hello'
+    return 'App Started'
 
 #task-two
 @app.route("/GetMetric",methods=['POST'])
 def getMetric():
 	metric = request.form['metric']
+	print(metric)
 	var requestOptions = {
     "method": "POST",
     "headers": {
         "Content-Type": "application/json"
     }
-};
+}
 
-var body = {
+var metricDB =  = {
     "type": "select",
     "args": {
         "table": "world_happ_rpt",
         "columns": [
             "*"
+        ],
+        "order_by": [
+            {
+                "column": "H_Rank",
+                "order": "asc"
+            }
         ]
     }
-};
+}
 
-requestOptions.body = JSON.stringify(body);
-
-fetchAction(url, requestOptions)
-.then(function(response) {
-	return response.json();
-})
-.then(function(result) {
-	console.log(result);
-})
-.catch(function(error) {
-	console.log('Request Failed:' + error);
-});
-
-@app.route('/Thiru-taskone')
-def hello_world():
-	users = requests.get('https://jsonplaceholder.typicode.com/users').json()
-	posts = requests.get('https://jsonplaceholder.typicode.com/posts').json()
-	return render_template('Task2.html',users=users,posts=posts)
-			
-	return "Done"
+    resp = requests.request("POST", hasuradbUrl, data=json.dumps(metricDB))
+    respObj = resp.json()
+	var actualMetric
+	
+	for i in len(respObj):
+	
+		actualMetric[i] = respObj[i][metric] #store the json data in an array
+	
+	return actualMetric #returns array
 
 app.run(debug = True,port=8080)
